f0ee474aab4ea6bd2fe8516b6009d10b424d77e7
Chaz Shrawder <chaz.shrawder@gmail.com>
2017-06-23 15:52:04 -0400

    AssemblyInfo changes

--- Removable-Media-Monitor/My Project/AssemblyInfo.vb
+++ Removable-Media-Monitor/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("17c4f4bc-4550-47e7-b432-e3f84f096a2b")> 
+<Assembly: Guid("17c4f4bc-4550-47e7-b432-e3f84f096a2b")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("2.0.1.0")>
+<Assembly: AssemblyFileVersion("2.0.1.0")>
